23ecc2f9a3fb4e7cede6623fb8935897019b9d28
iamsidar07 <ms8460149@gmail.com>
2023-09-06 21:14:55 +0530

    complete building

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-let todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")):["Start adding your todos!"]
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
 const todosList = document.getElementById("todos");
 const todoInput = document.getElementById("todoInput");
 const submitBtn = document.getElementById("addTodoBtn");
@@ -16,32 +16,56 @@ function addTodo(e) {
   e.preventDefault();
   if (!todoInput.value) return;
   let newTodo = todoInput.value;
-  todos.push(newTodo);
+  const todoObj = {
+    id: Date.now(),
+    isCompleted: false,
+    createdAt: new Date(),
+    todo: newTodo,
+  }
+  todos.push(todoObj);
   todoInput.value = "";
   updateTodo();
-  localStorage.setItem("todos",JSON.stringify(todos));
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
 function removeTodo(key) {
   todos = todos.filter((todo, i) => i !== key);
-  localStorage.setItem("todos",JSON.stringify(todos));
+  localStorage.setItem("todos", JSON.stringify(todos));
   updateTodo();
 }
 function editTodo(index) {
   enterTodoSection.style.display = "block";
-  todoInput.value = todos[index];
+  todoInput.value = todos[index].todo;
   removeTodo(index);
+}
+function toggleCompletionStatus(index) {
   
+  let todo = todos[index];
+  todo.isCompleted = todo.isCompleted ? false: true;
+  localStorage.setItem("todos", JSON.stringify(todos));
+  updateTodo();
 }
 function renderTodos() {
-  if(todos.length === 0) {
-    todos.push("Start adding your todos!");
+  if(todos.length===0){
+    todos.push({
+      createdAt:Date.now(),
+      id:1694012697590,
+      isCompleted:false,
+      todo:"Welcome to todo app. Start by creating a new todo! "
+    });
   }
-  todos?.reverse().map((todo, index) => {
+  const sortedTodos = todos.sort((a, b) => new Date(a.createdAt) > new Date(b.createdAt)?-1:1)
+  sortedTodos.map(({ todo, isCompleted, createdAt }, index) => {
     let currentHTML = todosList.innerHTML;
     let newHTML = `
-        <div class="todoItem">
-          <p>${index + 1}. ${todo}</p>
-          <div class="actions">
+        <div class="todoItem" style="${isCompleted?"box-shadow: 5px 7px skyblue;":"box-shadow: 5px 7px white"};">
+          <div>
+           <p style="${isCompleted ? "text-decoration: line-through; font-style: italic":""}">${index + 1}. ${todo}</p>
+           <small class="createdAt">${new Date(createdAt ? createdAt : updatedAt).toLocaleTimeString()}</small>
+          </div>
+          <div class="actions" title="${isCompleted?"Task completed":"Task not completed"}">
+            <div class="taskCompletionStatus ${isCompleted ? "taskCompleted" : "taskNotCompleted"}" onClick="toggleCompletionStatus(${index})">
+            <i class="fa-solid fa-check" style="${isCompleted ? "opacity:1;":"opacity:0;"}"></i>
+            </div>
             <i class="fa-solid fa-pen-nib icon editIcon" title="Edit todo" onClick="editTodo(${index})"></i>
             <i class="fa-solid fa-trash icon deleteIcon" title="Delete todo" onClick="removeTodo(${index})"></i>
           </div>
