83c21f9de1b3c641764d33e190c039ac27588e9f
iamsidar07 <ms8460149@gmail.com>
2023-09-06 18:16:54 +0530

    made todos persistent by adding localstorage

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-let todos = ["try out adding a new todo."];
+let todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")):["Start adding your todos!"]
 const todosList = document.getElementById("todos");
 const todoInput = document.getElementById("todoInput");
 const submitBtn = document.getElementById("addTodoBtn");
@@ -19,17 +19,24 @@ function addTodo(e) {
   todos.push(newTodo);
   todoInput.value = "";
   updateTodo();
+  localStorage.setItem("todos",JSON.stringify(todos));
 }
 function removeTodo(key) {
-  todos = todos.filter((todo, i) => i !== key)
+  todos = todos.filter((todo, i) => i !== key);
+  localStorage.setItem("todos",JSON.stringify(todos));
   updateTodo();
 }
 function editTodo(index) {
+  enterTodoSection.style.display = "block";
   todoInput.value = todos[index];
   removeTodo(index);
+  
 }
 function renderTodos() {
-  todos.reverse().map((todo, index) => {
+  if(todos.length === 0) {
+    todos.push("Start adding your todos!");
+  }
+  todos?.reverse().map((todo, index) => {
     let currentHTML = todosList.innerHTML;
     let newHTML = `
         <div class="todoItem">
